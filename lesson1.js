96051cbdf22a474f8615bc6fc19d4dccae2e6117
Taniya Maiti <taniyamaiti04@gmail.com>
2023-10-17 23:29:55 +0530

    first commit

---
+++ lesson1.js
@@ -0,0 +1,9 @@
+const accountid =123456
+let accemail ="myname@gmail.com"
+var accpassword ="34567"
+accity ="Bengaluru"
+//accountid = 2
+accemail="hi@gmail.com"
+console.log(accountid); 
+console.log(accemail); 
+console.table([accountid,accemail,accemail,accpassword,accity]);
